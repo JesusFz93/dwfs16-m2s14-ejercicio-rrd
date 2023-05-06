1ae1852b87adb6f19677105a6b14ac8746d53b2a
Jesus <jesus.fdz93@gmail.com>
2023-05-06 11:20:42 -0500

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+
+const App = () => {
+  return (
+    <>
+      <HomePage />
+      <AboutPage />
+    </>
+  );
+};
+
+export default App;
