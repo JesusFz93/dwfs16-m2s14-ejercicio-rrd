1ce114d5dd8f1c5b5949075f50334cec88874260
Jesus <jesus.fdz93@gmail.com>
2023-05-06 11:58:39 -0500

    react router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+
 const AppRouter = () => {
-  return <h1>AppRouter</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default AppRouter;
